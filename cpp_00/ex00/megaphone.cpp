1ee3d088752e507a66c79da9c9e543bcd7c31faf
apa247 <david.aparicio247@hotmail.com>
2024-09-05 21:14:15 +0200

    ex00

---
+++ cpp_00/ex00/megaphone.cpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: daparici <daparici@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/05 20:30:23 by daparici          #+#    #+#             */
+/*   Updated: 2024/09/05 20:50:24 by daparici         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	int i = 0;
+	int j;
+	
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else if (argc > 1)
+	{
+		while (argv[++i])
+		{
+			j = -1;
+			while (argv[i][++j])
+				std::cout << (char)std::toupper(argv[i][j]);
+		}
+		std::cout << std::endl;
+	}
+	return 0;
+}
