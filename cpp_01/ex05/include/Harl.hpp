8e67182ffad38b022b8db6d2fcd3b548215877aa
apa247 <david.aparicio247@hotmail.com>
2024-09-21 13:59:50 +0200

    mejorando ex05

--- cpp_01/ex05/include/Harl.hpp
+++ cpp_01/ex05/include/Harl.hpp
@@ -6,7 +6,28 @@
 /*   By: daparici <daparici@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/21 01:08:43 by daparici          #+#    #+#             */
-/*   Updated: 2024/09/21 01:08:44 by daparici         ###   ########.fr       */
+/*   Updated: 2024/09/21 13:57:46 by daparici         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <cstdio>
+#include <cstdlib>
+#include <map>
+
+class	Harl 
+{
+	public:
+		Harl();
+		~Harl();
+		void complain(std::string level);
+		
+	private:
+		void	debug(void);
+		void	info(void);
+		void	warning(void);
+		void	error(void);
+};
--- cpp_01/ex05/src/Harl.cpp
+++ cpp_01/ex05/src/Harl.cpp
@@ -6,7 +6,44 @@
 /*   By: daparici <daparici@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/21 01:08:51 by daparici          #+#    #+#             */
-/*   Updated: 2024/09/21 01:08:52 by daparici         ###   ########.fr       */
+/*   Updated: 2024/09/21 13:55:20 by daparici         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Harl.hpp"
+
+Harl::Harl() {}
+
+Harl::~Harl() {}
+
+void Harl::debug(void) {
+    std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special ketchup burger. I really do!" << std::endl;
+}
+
+void Harl::info(void) {
+    std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
+}
+
+void Harl::warning(void) {
+    std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
+}
+
+void Harl::error(void) {
+    std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+}
+
+void Harl::complain(std::string level) {
+    std::map<std::string, void (Harl::*)(void)> levels = {
+        {"1", &Harl::debug},
+        {"2", &Harl::info},
+        {"3", &Harl::warning},
+        {"4", &Harl::error}
+    };
+
+    auto it = levels.find(level);
+    if (it != levels.end()) {
+        (this->*(it->second))();
+    } else {
+        std::cout << "Unknown level: " << level << std::endl;
+    }
+}
