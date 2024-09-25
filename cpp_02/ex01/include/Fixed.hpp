771815c2f7a70ef76b4f7c72bde2d7e5758be8ef
apa247 <david.aparicio247@hotmail.com>
2024-09-25 21:24:11 +0200

    completando ex01

--- cpp_02/ex01/include/Fixed.hpp
+++ cpp_02/ex01/include/Fixed.hpp
@@ -5,25 +5,33 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: daparici <daparici@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/25 10:41:46 by daparici          #+#    #+#             */
-/*   Updated: 2024/09/25 17:58:41 by daparici         ###   ########.fr       */
+/*   Created: 2024/09/25 20:50:04 by daparici          #+#    #+#             */
+/*   Updated: 2024/09/25 21:10:51 by daparici         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 #include <iostream>
+#include <cmath>
 
-class Fixed {
+class	Fixed
+{
 	private:
-		int					fixedPointValue;
-		static const int	fractionalBits = 8;
-	
+		int					_raw;
+		static const int	_frac = 8;
+
 	public:
-		Fixed();
-		~Fixed();
+		Fixed(void);
+		~Fixed(void);
 		Fixed(Fixed const &src);
+		Fixed(const int value);
+		Fixed(const float value);
 		Fixed &operator=(const Fixed &rhs);
 		int getRawBits(void) const;
 		void setRawBits(int const raw);
+		float toFloat( void ) const;
+		int toInt( void ) const;
 };
+
+std::ostream	&operator<<(std::ostream &out, const Fixed &fixed);
