19629e25d239419a125e90e9feadc9d43b8369d9
apa247 <david.aparicio247@hotmail.com>
2024-09-26 22:37:00 +0200

    ex00 cpp03

---
+++ cpp_03/ex00/include/ClapTrap.hpp
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: daparici <daparici@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/26 22:31:44 by daparici          #+#    #+#             */
+/*   Updated: 2024/09/26 22:34:40 by daparici         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <iostream>
+#include <string>
+
+class	ClapTrap
+{
+	private:
+		std::string	_name;
+		int			_hitPoints;
+		int			_energyPoints;
+		int			_attackDamage;
+		
+	public:
+
+		// Cosntructor
+		ClapTrap(void);
+		ClapTrap(std::string name);
+		ClapTrap(ClapTrap const & src);
+		
+		// Destructor
+		~ClapTrap(void);
+
+		// Overload operator
+		ClapTrap & operator=(ClapTrap const & rhs);
+		void	attack(std::string const & target);
+		void	takeDamage(unsigned int amount);
+		void	beRepaired(unsigned int amount);
+};
