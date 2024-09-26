6b94803710c45e56304450cb928905dd69ca15f2
apa247 <david.aparicio247@hotmail.com>
2024-09-26 16:52:24 +0200

    cambiado ex01

--- cpp_02/ex01/src/Fixed.cpp
+++ cpp_02/ex01/src/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: daparici <daparici@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 21:08:29 by daparici          #+#    #+#             */
-/*   Updated: 2024/09/25 21:10:04 by daparici         ###   ########.fr       */
+/*   Updated: 2024/09/26 15:23:53 by daparici         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,58 @@
 Fixed::Fixed(void) : _raw(0)
 {
 	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const int value)
+{
+	std::cout << "Int constructor called" << std::endl;
+	this->_raw = value << this->_frac;
+}
+
+Fixed::Fixed(const float value)
+{
+	std::cout << "Float constructor called" << std::endl;
+	this->_raw = roundf(value * (1 << this->_frac));
+}
+
+Fixed::~Fixed() {
+	std::cout << "Destructor called" << std::endl;
+}
+
+Fixed::Fixed(Fixed const &other) {
+	std::cout << "Copy constructor called" << std::endl;
+	*this = other;
+}
+
+Fixed& Fixed::operator=(const Fixed &rhs) {
+	std::cout << "Copy assignment operator called" << std::endl;
+	if (this != &rhs)
+		this->_raw = rhs.getRawBits();
+	return *this;
+}
+
+
+int Fixed::getRawBits(void) const {
+	return this->_raw;
+} 
+
+void Fixed::setRawBits(int const raw) {
+	this->_raw = raw;
+}
+
+float Fixed::toFloat( void ) const
+{
+	return (float)this->_raw / (float)(1 << this->_frac);
+}
+
+int Fixed::toInt( void ) const
+{
+	return this->_raw >> this->_frac;
+}
+
+std::ostream	&operator<<(std::ostream &out, const Fixed &fixed)
+{
+	out << fixed.toFloat();
+	return out;
 }
 
