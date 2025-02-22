7de6bfcf93ad0cab30695630e14028271f23c1d7
apa247 <david.aparicio247@hotmail.com>
2025-02-22 20:15:21 +0100

    ex02 queda solo unafunción

--- cpp_09/ex02/include/PmergeMe.hpp
+++ cpp_09/ex02/include/PmergeMe.hpp
@@ -6,7 +6,26 @@
 /*   By: daparici <daparici@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/20 19:36:17 by daparici          #+#    #+#             */
-/*   Updated: 2025/02/20 19:36:18 by daparici         ###   ########.fr       */
+/*   Updated: 2025/02/20 19:45:17 by daparici         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
+
+# include <iostream>
+# include <vector>
+# include <deque>
+# include <algorithm>
+
+template <typename T, template <typename, typename> class Container>
+class PmergeMe
+{
+	public:
+		PmergeMe();
+		PmergeMe(Container<T, std::allocator<T> > const & src);
+		PmergeMe(PmergeMe const & src);
+		~PmergeMe();
+		PmergeMe & operator=(PmergeMe const & src);
+
+		Container<T, std::allocator<T> > sort(const Container<T, std::allocator<T> > & array) const;
+};
